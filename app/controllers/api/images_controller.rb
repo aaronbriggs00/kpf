4f97f3c32f150009a3396c2c4e8dad27e99e8408
AaronBriggs <aaron00.briggs@gmail.com>
2021-09-06 23:06:24 -0500

    cleans up image series in jsons

--- app/controllers/api/images_controller.rb
+++ app/controllers/api/images_controller.rb
@@ -1,16 +1,16 @@
 class Api::ImagesController < ApplicationController
   def create
-    image = Image.new(
+    @image = Image.new(
       image_url: params[:image_url],
       actor_id: params[:actor_id],
       group_id: params[:group_id],
       event_id: params[:event_id],
       location_id: params[:location_id],
     )
-    if image.save
-      render json: {message: "image uploaded successfully"}, status: :created
+    if @image.save
+      render "show.json.jb", status: :created
     else
-      render json: { errors: image.errors.full_messages }, status: :bad_request
+      render json: { errors: @image.errors.full_messages }, status: :bad_request
     end
   end
   def destroy
