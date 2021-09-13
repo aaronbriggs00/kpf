31fdeead83d0e63c24c351564ed93cba7ea48dd1
AaronBriggs <aaron00.briggs@gmail.com>
2021-09-13 03:40:53 -0500

    trying * url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080', 'https://pedantic-beaver-221b0f.netlify.app/'
+    origins '*'
 
     resource '*',
       headers: :any,
